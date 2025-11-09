b4d389353dba3035c844ddc364b83b798bb46e91
mdsaimon1232 <maybenafis@gmail.com>
2025-11-10 03:12:20 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// main.js
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "true";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1600,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webviewTag: true
+    },
+    title: "TriCompare (Browser)"
+  });
+
+  win.loadFile(path.join(__dirname, "index.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
